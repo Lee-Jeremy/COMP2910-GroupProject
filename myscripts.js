5c5e9720aabc111b00ee3c2b10bcae846ad1859f
Keir Forster <keirforster@gmail.com>
2016-04-29 23:15:40 -0700

    Fixed Logic Bugs

--- myscripts.js
+++ myscripts.js
@@ -1,12 +1,22 @@
 // Global Variables
-var count = 0;
-var array = [1, 2, 3, 4, 5, 6, 7, 8, 9]; // The Matrix
-var userSelection = [1, 2]; // The Two Cells Chosen By The User
+var count = 0; // Number of reveals across all matrix cells
+var r1c1Reveals = 0; // Number of reveals in the 1st matrix cell
+var r1c2Reveals = 0; //   						 2nd 
+var r1c3Reveals = 0; //   						 3rd
+var r2c1Reveals = 0; //   						 4th
+var r2c2Reveals = 0; //   						 5th
+var r2c3Reveals = 0; //   						 6th
+var r3c1Reveals = 0; //   						 7th
+var r3c2Reveals = 0; //   						 8th
+var r3c3Reveals = 0; //   						 9th
+var matrix = [1, 2, 3, 4, 5, 6, 7, 8, 9]; // The Matrix Values
+var userSelection = [1, 2]; // The Two User Selected Values
 var answer; // The Answer to the Equation
-var duplicates = 0;
-var timer;
-var i;
-var k;
+var numSolutions = 0; // Number of Solution Combinations in the Matrix
+var timer; // Timer
+var seconds = 1; // Seconds counter
+var i; // For-loop counter
+var k; // For-loop counter
 
 // Abbreviated getElementByID
 function $(id) {
@@ -17,314 +27,376 @@ function $(id) {
 	return element;			
 	}
 
+generateAnswer();
+// Create an answer
+function generateAnswer() {
+	answer = Math.floor((Math.random() * 9) + 1); // 1 to 9
+}
+
 fillMatrix();
 // Fill The Matrix Array
 function fillMatrix() {
-	for (i = 0; i < array.length; i++) {
-		var rand = Math.floor((Math.random() * 9) + 1); // 1 to 9
-		array[i] = rand;
+	for (i = 0; i < matrix.length; i++) {
+		var rand = Math.floor(Math.random() * 9); // 0 to 9
+		matrix[i] = rand;
 	}
 }
 
 check();
-// Check for duplicate cells
+// Check the Matrix for at least One solution
 function check() {
-	for (i = 0; i < array.length - 1; i++) {	
-		for (k = (i + 1); k < array.length; k++) {
-			if (array[i] == array[k]) {
-				duplicates++;
-				rand = Math.floor((Math.random() * 9) + 1); // 1 to 9
-				array[k] = rand; // Assign the second occurence a new random number			
+	for (i = 0; i < matrix.length - 1; i++) {	
+		for (k = (i + 1); k < matrix.length; k++) {
+			if ((matrix[i] + matrix[k]) == answer) { 
+				numSolutions++;						
+			} else if (numSolutions == 0){
+				rand = Math.floor(Math.random() * 9); // 0 to 9
+				matrix[k] = rand; // Assign the second value (k) a new random number		
 			} 				
 		}
 	}
-
-while (duplicates != 0) {
-	duplicates = 0;
+while (numSolutions == 0) { // Ensure the matrix contains at least one solution
 	check();
 }
-duplicates = 0;
+numSolutions = 0;
+}
+
+// Timer
+timer = setInterval(myTimer, 1000); // Execute Every 1 Second(s) 
+function myTimer() {	
+	seconds++;	
+	if (seconds == 2) { 
+		revealAnswer();
+	} else if (seconds == 4) {
+		hideFifth();
+		revealMatrix();
+	} else if (seconds == 8) {
+		revealAnswer();
+		hideMatrix();
+		clearInterval(timer); // Stop the timer		
+		seconds = 0; // Reset the seconds to 0 
+	} 
+}
+
+// Reveal Operator
+function revealOperator() {
+	var cell = $('second');
+	cell.innerHTML = "-";	
+}
+
+// Hide Operator
+function hideOperator() {
+	var cell = $('second');
+	cell.innerHTML = "";	
 }
 
 // Reveal Answer
-timer = setTimeout(revealFifth, 1500); // 1.5 Second Delay onload
-function revealFifth() {
-	var cell = $('fifth');
-	answer = (array[0] + array[3]);
+function revealAnswer() {
+	var cell = $('fifth');	
 	cell.innerHTML = answer;	
 }
 
 // Hide Answer
-timer = setTimeout(hideFifth, 3500); // 2 Second Delay after reveal
 function hideFifth() {
 	var cell = $('fifth');
 	cell.innerHTML = "";	
 }
 
 // Reveal Matrix
-timer = setTimeout(revealMatrix, 5500); // 2 Second Delay After Answer is Hidden
 function revealMatrix() {
-	var i;
-	var k;
 	var z = 0;
 	var cell;
-	
 	for (i = 1; i <= 3; i++) {
 		for (k = 1; k <= 3; k++) {
-			cell = $('r' + i + 'c' + k).innerHTML = array[z]; // Reveal each cell
+			cell = $('r' + i + 'c' + k).innerHTML = matrix[z]; // Reveal each value in their div's
 			z++;
 		}
 	}
 }
 
 // Hide Matrix
-timer = setTimeout(hideMatrix, 7500); // 2 Second Delay After Matrix is Revealed
 function hideMatrix() {
-	var i;
-	var k;
 	var cell;
-	
 	for (i = 1; i <= 3; i++) {
 		for (k = 1; k <= 3; k++) {
-			cell = $('r' + i + 'c' + k).innerHTML = ""; // Hide each cell
+			cell = $('r' + i + 'c' + k).innerHTML = ""; // Remove each value from their div's
 		}
 	}	
 }
 
-// Reveal Operator
-timer = setTimeout(revealOperator, 9500); // 2 Second Delay After Matrix is Hidden
-function revealOperator() {
-	var cell = $('second');
-	cell.innerHTML = "+";	
-}
-
 // Reveal R1C1 Value
 function revealR1C1(id) {
-	var cell = ($(id));
-	var num = array[0];		
-	count++;
+	var cell = ($(id)); // First matrix cell
+	var num = matrix[0]; // First value of matrix array  	
 	
-	if (count == 1) {
-		cell.innerHTML = num;
-		cell.style.backgroundColor = "#800080"; // Purple
-		$('first').innerHTML = num;
-		$('first').style.backgroundColor = "#800080"; 
-		userSelection[0] = num;
+	if (seconds == 0 && r1c1Reveals != 1) { // Prevents the function from executing before the intro reveals have
+		count++; 						   // finished and from executing more that once on same cell
+	}
+	
+	if (count == 1 && r1c1Reveals != 1) {
+		cell.innerHTML = num; // Copy first value of matrix array to first matrix cell and reveal
+		cell.style.backgroundColor = "#800080"; // Change background color of first matrix cell to purple
+		$('first').innerHTML = num; // Copy value of first matrix cell to first equation cell and reveal
+		$('first').style.backgroundColor = "#800080"; // Change background color of first equation cell to purple
+		userSelection[0] = num; // Copy value of first matrix cell to answer array at index 0
+		r1c1Reveals++; // Increment the fucntion execution counter
 	}	
 	
-	if (count == 2) {
+	if (count == 2 && r1c1Reveals != 1) { 
 		cell.innerHTML = num;
-		cell.style.backgroundColor = "#800080"; 
+		cell.style.backgroundColor = " #4169E1"; // Blue
 		$('third').innerHTML = num;
-		$('third').style.backgroundColor = "#800080"; 
-		userSelection[1] = num;
-		checkAnswer();
+		$('third').style.backgroundColor = " #4169E1"; // Blue
+		userSelection[1] = num; 
+		r1c1Reveals++; 
+		checkEquation(); // Check if the equation is true
 	}
 }
 
 // Reveal R1C2 Value
 function revealR1C2(id) {
 	var cell = ($(id));
-	var num = array[1];
-	count++;
+	var num = matrix[1];
 	
-	if (count == 1) {
+	if (seconds == 0 && r1c2Reveals != 1) {
+		count++;
+	}
+	
+	if (count == 1 && r1c2Reveals != 1) {
 		cell.innerHTML = num;	
 		cell.style.backgroundColor = "#800080"; 
 		$('first').innerHTML = num;
 		$('first').style.backgroundColor = "#800080"; 
 		userSelection[0] = num;
+		r1c2Reveals++; 
 	}	
 	
-	if (count == 2) {
+	if (count == 2 && r1c2Reveals != 1) {
 		cell.innerHTML = num;	
-		cell.style.backgroundColor = "#800080"; 
+		cell.style.backgroundColor = "#4169E1"; 
 		$('third').innerHTML = num;
-		$('third').style.backgroundColor = "#800080"; 
+		$('third').style.backgroundColor = "#4169E1"; 
 		userSelection[1] = num;
-		checkAnswer();
+		r1c2Reveals++; 
+		checkEquation();	
 	}
 }
 
 // Reveal R1C3 Value
 function revealR1C3(id) {
 	var cell = ($(id));
-	var num = array[2];
-	count++;
+	var num = matrix[2];
 	
-	if (count == 1) {
+	if (seconds == 0 && r1c3Reveals != 1) {
+		count++;
+	}
+	
+	if (count == 1 && r1c3Reveals != 1) {
 		cell.innerHTML = num;	
 		cell.style.backgroundColor = "#800080";
 		$('first').innerHTML = num;
 		$('first').style.backgroundColor = "#800080"; 
 		userSelection[0] = num;
+		r1c3Reveals++;
 	}	
 	
-	if (count == 2) {
+	if (count == 2 && r1c3Reveals != 1) {
 		cell.innerHTML = num;	
-		cell.style.backgroundColor = "#800080"; 
+		cell.style.backgroundColor = "#4169E1"; 
 		$('third').innerHTML = num;
-		$('third').style.backgroundColor = "#800080";
+		$('third').style.backgroundColor = "#4169E1";
 		userSelection[1] = num;
-		checkAnswer();
+		r1c3Reveals++;
+		checkEquation();
 	}
 }
 
 // Reveal R2C1 Value
 function revealR2C1(id) {
 	var cell = ($(id));
-	var num = array[3];
-	count++;
+	var num = matrix[3];
 	
-	if (count == 1) {
+	if (seconds == 0 && r2c1Reveals != 1) {
+		count++;
+	}
+	
+	if (count == 1 && r2c1Reveals != 1) {
 		cell.innerHTML = num;
 	cell.style.backgroundColor = "#800080";		 
 		$('first').innerHTML = num;
 		$('first').style.backgroundColor = "#800080"; 
 		userSelection[0] = num;
+		r2c1Reveals++;
 	}	
 	
-	if (count == 2) {
+	if (count == 2 && r2c1Reveals != 1) {
 		cell.innerHTML = num;
-		cell.style.backgroundColor = "#800080"; 
+		cell.style.backgroundColor = "#4169E1"; 
 		$('third').innerHTML = num;
-		$('third').style.backgroundColor = "#800080"; 
+		$('third').style.backgroundColor = "#4169E1"; 
 		userSelection[1] = num;
-		checkAnswer();
+		r2c1Reveals++;
+		checkEquation();
 	}
 }
 
 // Reveal R2C2 Value
 function revealR2C2(id) {
 	var cell = ($(id));
-	var num = array[4];
-	count++;
+	var num = matrix[4];
 	
-	if (count == 1) {
+	if (seconds == 0 && r2c2Reveals != 1) {
+		count++;
+	}
+	
+	if (count == 1 && r2c2Reveals != 1) {
 		cell.innerHTML = num;
 		cell.style.backgroundColor = "#800080"; 
 		$('first').innerHTML = num;
 		$('first').style.backgroundColor = "#800080"; 
 		userSelection[0] = num;
+		r2c2Reveals++;
 	}	
 
-	if (count == 2) {
+	if (count == 2 && r2c2Reveals != 1) {
 		cell.innerHTML = num;
-		cell.style.backgroundColor = "#800080"; 
+		cell.style.backgroundColor = "#4169E1"; 
 		$('third').innerHTML = num;
-		$('third').style.backgroundColor = "#800080"; 
+		$('third').style.backgroundColor = "#4169E1"; 
 		userSelection[1] = num;
-		checkAnswer();
+		r2c2Reveals++;
+		checkEquation();
 	}	
 }
 
 // Reveal R2C3 Value
 function revealR2C3(id) {
 	var cell = ($(id));
-	var num = array[5];	
-	count++;
+	var num = matrix[5];	
 	
-	if (count == 1) {
+	if (seconds == 0 && r2c3Reveals != 1) {
+		count++;
+	}
+	
+	if (count == 1 && r2c3Reveals != 1) {
 		cell.innerHTML = num;
 		cell.style.backgroundColor = "#800080"; 
 		$('first').innerHTML = num;
 		$('first').style.backgroundColor = "#800080"; 
 		userSelection[0] = num;
+		r2c3Reveals++;
 	}	
 	
-	if (count == 2) {
+	if (count == 2 && r2c3Reveals != 1) {
 		cell.innerHTML = num;	
-		cell.style.backgroundColor = "#800080";
+		cell.style.backgroundColor = "#4169E1";
 		$('third').innerHTML = num;
-		$('third').style.backgroundColor = "#800080"; 
+		$('third').style.backgroundColor = "#4169E1"; 
 		userSelection[1] = num;
-		checkAnswer();
+		r2c3Reveals++;
+		checkEquation();
 	}
 }
 
 // Reveal R3C1 Value
 function revealR3C1(id) {
 	var cell = ($(id));
-	var num = array[6];	
-	count++;
+	var num = matrix[6];	
 	
-	if (count == 1) {
+	if (seconds == 0 && r3c1Reveals != 1) {
+		count++;
+	}
+	
+	if (count == 1 && r3c1Reveals != 1) {
 		cell.innerHTML = num;
 		cell.style.backgroundColor = "#800080"; 
 		$('first').innerHTML = num;
 		$('first').style.backgroundColor = "#800080"; 
 		userSelection[0] = num;
+		r3c1Reveals++;
 	}
 	
-	if (count == 2) {
+	if (count == 2 && r3c1Reveals != 1) {
 		cell.innerHTML = num;
-		cell.style.backgroundColor = "#800080"; 
+		cell.style.backgroundColor = "#4169E1"; 
 		$('third').innerHTML = num;
-		$('third').style.backgroundColor = "#800080"; 
+		$('third').style.backgroundColor = "#4169E1"; 
 		userSelection[1] = num;
-		checkAnswer();
+		r3c1Reveals++;
+		checkEquation();
 	}
 }
 
 // Reveal R3C2 Value
 function revealR3C2(id) {
 	var cell = ($(id));
-	var num = array[7];
-	count++;
+	var num = matrix[7];
 	
-	if (count == 1) {
+	if (seconds == 0 && r3c2Reveals != 1) {
+		count++;
+	}
+	
+	if (count == 1 && r3c2Reveals != 1) {
 		cell.innerHTML = num;
 		cell.style.backgroundColor = "#800080"; 
 		$('first').innerHTML = num;
 		$('first').style.backgroundColor = "#800080"; 
 		userSelection[0] = num;
+		r3c2Reveals++;
 	}	
 	
-	if (count == 2) {
+	if (count == 2 && r3c2Reveals != 1) {
 		cell.innerHTML = num;
-		cell.style.backgroundColor = "#800080"; 
+		cell.style.backgroundColor = "#4169E1"; 
 		$('third').innerHTML = num;
-		$('third').style.backgroundColor = "#800080"; 
+		$('third').style.backgroundColor = "#4169E1"; 
 		userSelection[1] = num;
-		checkAnswer();
+		r3c2Reveals++;
+		checkEquation();
 	}
 }
 
 // Reveal R3C3 Value
 function revealR3C3(id) {
 	var cell = ($(id));
-	var num = array[8];	
-	count++;
+	var num = matrix[8];	
 	
-	if (count == 1) {
+	if (seconds == 0 && r3c3Reveals != 1) {
+		count++;
+	}
+	
+	if (count == 1 && r3c3Reveals != 1) {
 		cell.innerHTML = num;
 		cell.style.backgroundColor = "#800080"; 
 		$('first').innerHTML = num;
 		$('first').style.backgroundColor = "#800080"; 
 		userSelection[0] = num;
+		r3c3Reveals++;
 	}
 	
-	if (count == 2) {
+	if (count == 2 && r3c3Reveals != 1) {
 		cell.innerHTML = num;
-		cell.style.backgroundColor = "#800080"; 
+		cell.style.backgroundColor = "#4169E1"; 
 		$('third').innerHTML = num;
-		$('third').style.backgroundColor = "#800080"; 
+		$('third').style.backgroundColor = "#4169E1"; 
 		userSelection[1] = num;
-		checkAnswer();
+		r3c3Reveals++;
+		checkEquation();
 	}
 }
 
-// Check Answer
-function checkAnswer(){
+// Check Equation
+function checkEquation(){
 	var first = userSelection[0];
 	var second = userSelection[1];
 	
 	if ((first + second) == answer){
-		revealFifth();
+		revealAnswer();
 		$('fifth').style.backgroundColor = "#29a329"; // Green
 		
 	} else {
 		$('fifth').style.backgroundColor = "#e60000"; // Red
+		setTimeout(revealMatrix, 500) // Delay Matrix Reveal by 0.5 seconds
 	}
 }
 

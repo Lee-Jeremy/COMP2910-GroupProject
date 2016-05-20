9584d89d6fd24528faa289e44c3d1d5f1aa2ca3e
Keir Forster <keirforster@gmail.com>
2016-05-20 13:01:51 -0700

    SetLevel Changes

--- Version_1.3/scripts/set_level.js
+++ Version_1.3/scripts/set_level.js
@@ -44,7 +44,7 @@ function fillMatrix() {
 			// Don't allow duplicate values in the matrix
 			while (num == matrix[0] || num == matrix[1] || num == matrix[2] || num == matrix[3] || num == matrix[4] 
 			|| num == matrix[5] || num == matrix[6] || num == matrix[7] || num == matrix[8]) {
-				num = Math.floor((Math.random() * divisionCardValueMax) + divisionCardValueMin);
+				num = Math.floor((Math.random() * cardValueMax) + cardValueMin);
 			}
 		matrix[i] = num; // Place num into the matrix array
 		}

4aa6e5386e109f00d151d713b70396e6d5213b1b
Jeremy Lee <jeremylee.business@gmail.com>
2016-05-25 23:14:21 -0700

    Achievement tooltip completed

--- Version_1.3/account.php
+++ Version_1.3/account.php
@@ -22,20 +22,54 @@
                         <img src="./images/locked.png" alt="locked">
                     </div>
                     <div id="achieve1Txt" class="achieveTxt">
-                        <h3>Flash Memory</h3>
-                        <p>Clear first 10 stages without losing any multipliers</p>
+                        <table>
+                            <tr>
+                                <td><img src="./images/achieve1.png" alt="achieve1"></td>
+                                <td id="achieve1Title">Flash Memory</td>
+                            </tr>
+                            <tr>
+                                <td class="achieveDescription" colspan="2">
+                                Clear first 10 stages without losing any multipliers
+                                </td>
+                            </tr>
+                        </table>
                     </div>
                 </td>
                 <td id="achieve2Image" class="achieveImage"><img src="./images/achieve2.png" alt="achieve2" onclick="acheiveUnlock(2)">
                     <div id="achieve2Status" class="achieveStatus">
                         <img src="./images/locked.png" alt="locked">
                     </div>
-                    <div id="achieve2Txt" class="achieveTxt">Achievement2 tooltip</div>
+                    <div id="achieve2Txt" class="achieveTxt">
+                        <table>
+                            <tr>
+                                <td><img src="./images/achieve2.png" alt="achieve2"></td>
+                                <td id="achieve2Title">Diehard</td>
+                            </tr>
+                            <tr>
+                                <td class="achieveDescription" colspan="2">
+                                Clear 15 stages with all 3 lives
+                                </td>
+                            </tr>
+                        </table>
+                    </div>
                 </td>
                 <td id="achieve3Image" class="achieveImage"><img src="./images/achieve3.png" alt="achieve3" onclick="acheiveUnlock(3)">
-                    <div id="achieve3Status" class="achieveStatus"><img src="./images/locked.png" alt="locked">
+                    <div id="achieve3Status" class="achieveStatus">
+                        <img src="./images/locked.png" alt="locked">
                     </div>
-                    <div id="achieve3Txt" class="achieveTxt">Achievement3 tooltip</div>
+                    <div id="achieve3Txt" class="achieveTxt">
+                        <table>
+                            <tr>
+                                <td><img src="./images/achieve3.png" alt="achieve3"></td>
+                                <td id="achieve3Title">Human Calculator</td>
+                            </tr>
+                            <tr>
+                                <td class="achieveDescription" colspan="2">
+                                Clear 20 stages
+                                </td>
+                            </tr>
+                        </table>
+                    </div>
                 </td>
             </tr>
         </table>
